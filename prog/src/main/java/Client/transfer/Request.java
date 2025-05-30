3d34c636b01866274975c091336455c5a7a888b8
Сушко Александр Александрович <cyberalexandr2@gmail.com>
2025-05-30 15:37:46 +0300

    Delete prog directory

--- prog/src/main/java/Client/transfer/Request.java
+++
@@ -1,8 +0,0 @@
-package Client.transfer;
-
-/**
- * Request - record, служащий для хранения данных запроса
- * @param command - название команды
- * @param arg - аргумент команды
- */
-public record Request(String command, String arg) {}
--- prog/src/main/java/Client/transfer/Response.java
+++
@@ -1,8 +0,0 @@
-package Client.transfer;
-
-/**
- * Response - record, служащий для хранения данных ответа
- * @param message - возвращаемое сообщение
- * @param args - доп. информация
- */
-public record Response(String message, String args) {}
